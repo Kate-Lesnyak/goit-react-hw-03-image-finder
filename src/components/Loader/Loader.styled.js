2621851229eb4e22d63f948b143f92d4c6ec40c0
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-03-06 18:40:37 +0200

    add component

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -10,4 +10,10 @@ background: ${({ theme }) => theme.colors.backgroundLoaderColor};
 display: flex;
 align-items: center;
 justify-content: center;
+/* 
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99; */
 `;
